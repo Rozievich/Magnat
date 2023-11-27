1880ae44cc7e65ba721bccf34e084ac1da47c0fa
rozievich <blackcoder459@outlook.com>
2023-11-27 12:45:09 +0500

    Jazzmin add for Magnat Project

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ ALLOWED_HOSTS = ['*']
 
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -19,7 +20,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # My apps
     'magnat.apps.MagnatConfig',
-
+    # External packages
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -92,3 +94,89 @@ MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+JAZZMIN_SETTINGS = {
+    "site_title": "Magnat",
+
+    "site_header": "Magnat",
+
+    "site_brand": "Magnat",
+
+    "site_logo": None,
+
+    "login_logo": None,
+
+    "login_logo_dark": None,
+
+    "site_logo_classes": "img-circle",
+
+    "site_icon": None,
+
+    "welcome_sign": "Admin page for Magnat IT company",
+
+    "copyright": "Magnat IT Company LTD",
+
+    "search_model": ['auth.User', 'account.Account', 'account.Media'],
+
+    "user_avatar": None,
+    "show_ui_builder": True,
+    "topmenu_links": [
+        {"name": "Home", "url": "admin:index",
+            "permissions": ["auth.view_user"]},
+
+        {"name": "Support", "url": "https://t.me/Rozievich", "new_window": True},
+
+        {"model": "auth.User"},
+
+        {"app": "foods"},
+    ],
+
+    "usermenu_links": [
+        {"name": "Support", "url": "https://t.me/Rozievich", "new_window": True},
+        {"model": "auth.user"},
+    ],
+
+    "show_sidebar": True,
+
+    "navigation_expanded": True,
+
+    "hide_apps": [],
+
+    "hide_models": ['auth.Group'],
+
+    "order_with_respect_to": ["auth", "books", "books.author", "books.book"],
+
+    "custom_links": {
+        "books": [{
+            "name": "Make Messages",
+            "url": "make_messages",
+            "icon": "fas fa-comments",
+            "permissions": ["books.view_book"]
+        }]
+    },
+
+    "icons": {
+        "auth": "fas fa-users-cog",
+        "auth.user": "fas fa-user",
+        "auth.Group": "fas fa-users",
+    },
+    "default_icon_parents": "fas fa-chevron-circle-right",
+    "default_icon_children": "fas fa-circle",
+
+    "related_modal_active": False,
+
+    "custom_css": None,
+    "custom_js": None,
+    "use_google_fonts_cdn": True,
+    "show_ui_builder": True,
+
+    "changeform_format": "vertical_tabs",
+    "changeform_format_overrides": {"auth.user": "collapsible", "auth.group": "vertical_tabs"},
+    "language_chooser": False,
+}
+
+JAZZMIN_UI_TWEAKS = {
+    "theme": "simplex",
+    "light_mode_theme": "light",
+}
